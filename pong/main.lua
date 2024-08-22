01bcd1e54b3dca64c4f2a429aeeee29a47dc424e
Peter Strawn <peter.strawn@jesuithighschool.org>
2024-08-22 13:49:20 -0700

    pong-7

--- pong/main.lua
+++ pong/main.lua
@@ -15,6 +15,8 @@ PADDLE_SPEED = 200
 function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest")
 
+    love.window.setTitle('Pong')
+
     math.randomseed(os.time())
 
     smallFont = love.graphics.newFont('font.ttf', 8)
@@ -28,6 +30,10 @@ function love.load()
         vsync = true
     })
 
+    -- track scores
+    player1Score = 0
+    player2Score = 0
+
     -- create game object
     player1 = Paddle(10, 30, 5, 20) 
     player2 = Paddle(VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 30, 5, 20)
@@ -38,6 +44,39 @@ function love.load()
 end
 
 function love.update(dt)
+    if gameState == 'play' then 
+        if ball:collides(player1) then 
+            ball.dx = -ball.dx * 1.03
+            ball.x = player1.x + 5
+
+            if ball.dy < 0 then 
+                ball.dy = -math.random(10, 150)
+            else 
+                ball.dy = math.random(10, 150)
+            end
+        end
+        if ball:collides(player2) then 
+            ball.dx = -ball.dx * 1.03
+            ball.x = player2.x - 4
+
+            if ball.dy < 0 then 
+                ball.dy = -math.random(10, 150)
+            else 
+                ball.dy = math.random(10, 150)
+            end
+        end
+
+        if ball.y <= 0 then 
+            ball.y = 0 
+            ball.dy = -ball.dy 
+        end
+
+        if ball.y >= VIRTUAL_HEIGHT - 4 then 
+            ball.y = VIRTUAL_HEIGHT - 4 
+            ball.dy = -ball.dy 
+        end
+    end
+
     -- player 1 movement
     if love.keyboard.isDown('w') then 
         player1.dy = -PADDLE_SPEED
